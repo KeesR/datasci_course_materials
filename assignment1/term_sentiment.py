6f6fd82a2d495dde3ed82472463676d53baa2e4b
KeesR <kees@home.nl>
2014-07-09 16:18:34 +0200

    Revert "Updated python files for Assignment 1. All work, and resulted in max score by grader"
    This reverts commit 69ad85108eafae26fbb8b91fb049c0dcb9564a09.

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,80 +1,17 @@
 import sys
-import json
 
-def load_sentiments(sent_file):
-	scores = {} # initialize an empty dictionary
-	for line in sent_file:
-		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-		scores[term] = int(score)  # Convert the score to an integer.
-	#print scores.items() # Print every (term, score) pair in the dictionary
-	return scores
-	
-def print_ustring(unicode_string):
-	encoded_string = unicode_string.encode('utf-8')
-	print encoded_string
-	
-def cleanup_words(words):
-	clean_words = []
-	for w in words:
-		#	remove hyperlinks
-		if w[0:7] == "http://":	continue
-		#	remove Twitter users
-		if w[0] == '@': continue
-		#	remove spurious hash tag
-		if w[0] == '#': continue
-		#	strip . comma colon semi colon question/exclamation mark from end of word
-		w = w.rstrip('..,:;!?')
-		#	rest is OK
-		clean_words.append(w)
-	return clean_words
-		
-def score_text(str, score_table, histogram):
-	words = cleanup_words(str.lower().split())
-	score = 0
-	other_words = []
-	for w in words:
-		if w in score_table:
-			score = score + score_table[w]
-		else:
-			other_words.append(w)
-	#	now add score to every other_word in histogram
-	for w in other_words:
-		if w in histogram:
-			histogram[w].append(score)
-		else:
-			histogram[w] = [score]
-	
-def process_tweets(tweet_file, score_table, histogram):
-	for line in tweet_file:
-		tweet = json.loads(line)
-		if 'text' in tweet:
-			#print_ustring(tweet['text']);
-			encoded_string = tweet['text'].encode('utf-8')
-			score_text(encoded_string, score_table, histogram)
-			#print encoded_string, "\n", other_words
-		#else:
-			#print "+++ Ill-formed tweet?"
-			#print tweet
+def hw():
+    print 'Hello, world!'
 
-def average(list):
-	sum = 0
-	for elem in list:
-		sum += elem
-	return sum*1.0/len(list)
-	
-def average_hist_lists(hist):
-	for k in hist:
-		hist[k] = average(hist[k])
-		
+def lines(fp):
+    print str(len(fp.readlines()))
+
 def main():
-	sent_file = open(sys.argv[1])
-	tweet_file = open(sys.argv[2])
-	scores = load_sentiments(sent_file)
-	histogram = {}	# initialize an empty dictionary
-	process_tweets(tweet_file, scores, histogram)
-	average_hist_lists(histogram)
-	for k in sorted(histogram, key=histogram.get, reverse=True):
-		print k, histogram[k]
+    sent_file = open(sys.argv[1])
+    tweet_file = open(sys.argv[2])
+    hw()
+    lines(sent_file)
+    lines(tweet_file)
 
 if __name__ == '__main__':
-	main()
+    main()
--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,43 +1,17 @@
 import sys
-import json
 
-def load_sentiments(sent_file):
-	scores = {} # initialize an empty dictionary
-	for line in sent_file:
-		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-		scores[term] = int(score)  # Convert the score to an integer.
-	#print scores.items() # Print every (term, score) pair in the dictionary
-	return scores
-	
-def print_ustring(unicode_string):
-	encoded_string = unicode_string.encode('utf-8')
-	print encoded_string
-	
-def score_text(str, score_table):
-	words = str.lower().split()
-	score = 0
-	for w in words:
-		if w in score_table:
-			score = score + score_table[w]
-	return score
-	
-def process_tweets(tweet_file, score_table):
-	for line in tweet_file:
-		tweet = json.loads(line)
-		if 'text' in tweet:
-			#print_ustring(tweet['text']);
-			encoded_string = tweet['text'].encode('utf-8')
-			score = score_text(encoded_string, score_table)
-			print score
-		#else:
-			#print "+++ Ill-formed tweet?"
-			#print tweet
+def hw():
+    print 'Hello, world!'
+
+def lines(fp):
+    print str(len(fp.readlines()))
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    scores = load_sentiments(sent_file)
-    process_tweets(tweet_file, scores)
+    hw()
+    lines(sent_file)
+    lines(tweet_file)
 
 if __name__ == '__main__':
     main()
